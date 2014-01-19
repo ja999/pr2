e86c2bd82c4b26a15173832fea1762f29cbcf44b
MiS27 <michal.slusarczyk@student.put.poznan.pl>
2014-01-19 16:29:44 +0100

    countStringInBase

--- pr.cpp
+++ pr.cpp
@@ -7,11 +7,22 @@ using namespace std;
 
 int countStringInBase(string str, string base) {
 	int count=0;
-	for (int i=0; i<base.length();i++) {
+	if (base.length() < str.length() || str.length() == 0)
+		return 0;
+	for (int i=0; i<= base.length() - str.length();i++) {
+		bool ok=true;
+		for (int j=0; j<str.length(); j++) {
+			if (base[i+j] != str[j]) {
+				ok=false;
+				break;
+			}
+		}
+		count+=ok;
 	}
 	return count;
 }
-int main(){
+int main(int argc, char* argv[]) { 
+	cout<<countStringInBase("t","")<<endl;
 	printf("kurwa...\n");
 	return 0;
 }
