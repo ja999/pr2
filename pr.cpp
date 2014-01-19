bcacf0b203537226ce2b25f6fc3674112c00d2bd
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-01-19 16:27:56 +0100

    adding file reading, firing the count method, hoping for no conflicts is insane

--- pr.cpp
+++ pr.cpp
@@ -3,17 +3,33 @@
 #include <time.h>
 #include <stdio.h>
 #include <omp.h>
+#include <vector>
+
+#define pb push_back
 
 using namespace std;
 
 int countStringInBase(string str, string base) {
 	int count = 0;
-	for(int i = 0; i < base.lenght(); i++) {
+	for(int i = 0; i < base.size(); i++) {
 	}
 	return count;
 }
 
 int main() {
-	printf("kurwa...\n");
+	fstream base_file, match_file;
+	string base, tmp;
+	vector<string> to_match;
+	base_file.open("base.txt", ios_base::in);
+	match_file.open("strings.txt", ios_base::in);
+	getline(base_file, base);
+	while(!match_file.eof()){
+		getline(match_file, tmp);
+		to_match.pb(tmp);
+	}
+	cout<<base.size()<<endl;
+	for(int i = 0; i < to_match.size(); i++){
+		cout<<countStringInBase(to_match[i], base)<<' ';
+	}
 	return 0;
 }
